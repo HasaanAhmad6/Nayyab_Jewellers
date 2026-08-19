ac12291d40d645581e77ddf99a2aeb18b9c89c85
HasaanAhmad6 <hasaanahmad6@users.noreply.github.com>
2026-08-19 18:15:50 +0500

    ci: configure GitHub Pages deployment with GitHub Actions and update base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Nayyab-Jewellers/',
+  base: '/Nayyab_Jewellers/',
   plugins: [react()],
 })
